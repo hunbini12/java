4d592d7ed701b658ecb0c47e0dec8c999be8f813
김현빈 <@DESKTOP-D9BHDVK>
2023-06-26 15:46:45 +0900

    스캐너

---
+++ chapter1/src/chapter1/Ex26.java
@@ -0,0 +1,26 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class Ex26 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("상품 정보를 입력하세요.");
+		
+		System.out.print("이름:");
+		String name = scanner.next();
+		
+		System.out.print("가격:");
+		int price = scanner.nextInt();
+		
+		System.out.println("재고량:");
+		int countStock = scanner.nextInt();
+		
+		System.out.println(name + ":" + price + ":" + countStock);
+		scanner.close();
+
+	}
+
+}
